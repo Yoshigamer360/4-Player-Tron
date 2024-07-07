8fda8d1ce02314199c780554a32c137557adf3d2
Yoshigamer360 <133695386+Yoshigamer360@users.noreply.github.com>
2024-07-07 11:46:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,3 +42,22 @@ class Player:
         if self.alive:
             self.x += self.bearing[0]
             self.y += self.bearing[1]
+
+# New game function - to create players
+def newGame():
+    newP1 = Player(50, height / 2, (2, 0), colourPlayer1)
+    newP2 = Player(width - 50, height / 2, (-2, 0), colourPlayer2)
+    newP3 = Player(width / 2, height - 50, (0, -2), colourPlayer3)
+    newP4 = Player(width / 2, 50, (0, 2), colourPlayer4)
+    return newP1, newP2, newP3, newP4
+
+# Set up the game window
+width, height = 600, 600
+offset = 0
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Tron by Yoshi_Gamer_360")
+
+# Font and clock
+font = pygame.font.SysFont("Centaur", 72)
+clock = pygame.time.Clock()
+checkTime = time.time()
